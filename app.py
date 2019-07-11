f2c2168cd15a500ef70b8442cf101399c58fbfac
szgk <szgk123@gmail.com>
2019-07-11 23:58:57 +0900

    test: add import modules

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify, make_response
+from flask_cors import CORS, cross_origin
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# use chrome_driver(ver74)
+import chromedriver_binary
+import os, datetime, json
+
+from .models import WebSite, Image
+from .modules.Colors import Colors
 app = Flask(__name__)
 
 @app.route('/')
